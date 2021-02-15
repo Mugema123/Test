5c303867e1172664a9c8cdb33f4792ab5c23932a
Mugema123 <mugemaleo@gmail.com>
2021-02-15 22:12:48 +0200

    ch-(frontendreact):work

---
+++ REACTJS/myforms/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import dashboard from './components/Dashboard'
+import './App.css';
+import {BrowserRouter as Router, Switch, Route  } from 'react-router-dom'
+
+function App() {
+  return (
+    < Router>
+    <div className="form">
+      
+     <Switch>
+     <Route path="/" exact component={SignUp} />
+     <Route path="/signin" component={SignIn} />
+     <Route path="/dashboard" component={dashboard} />
+      
+     </Switch>
+     
+    </div>
+    </Router>
+
+    
+  );
+}
+
+
+export default App;
